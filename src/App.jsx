af7dd7c3da3a47abb811b05ee9699323f7ab5f93
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-09-19 10:07:48 -0400

    basic background change and navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useState} from "react"
+import Background from "./components/background/background"
+import Navbar from "./components/navbar/navbar"
+import Hero from "./components/hero/hero"
 
 const App = () => {
+  let heroData = [
+    {text1: "Dive into", text2: "what you love"},
+    {text1: "Indulge", text2: "your passions"},
+    {text1: "Give in to", text2: "your passions"},
+  ]
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false)
   return (
-    <div></div>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar />
+      <Hero
+      setPlayStatus={setPlayStatus}
+      heroData={heroData[heroCount]}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}/>
+    </div>
   )
 }
 
